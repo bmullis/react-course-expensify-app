b9e62c12398ce9fefb947ba9e17785fd5ed89d4d
Brian Mullis <brian@smartrent.com>
2019-01-14 06:16:34 -0700

    Remove dummy expense data.

--- src/app.js
+++ src/app.js
@@ -12,17 +12,7 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 12300, createdAt: moment('2019-01-07') }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 14300, createdAt: moment('2019-01-03') }));
-store.dispatch(addExpense({ description: 'Kombucha', amount: 380, createdAt: moment('2019-01-09') }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: moment('2019-01-11') }));
-
 store.dispatch(setTextFilter(''));
-
-const state = store.getState();
-const visibileExpenses = getVisibileExpenses(state.expenses, state.filters);
-store.subscribe(() => { console.log(store.getState())});
-
 
 const jsx = (
     <Provider store={store}>
